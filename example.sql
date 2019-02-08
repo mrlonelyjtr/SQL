d3911d0f5ac763b0f3564d4ce8c631717fc125c1
Ma Tianyao <mrlonelyjtr@icloud.com>
2019-02-08 23:42:43 +0800

    add example.sql

---
+++ example.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE shop;
+
+CREATE TABLE Product
+(
+    product_id      CHAR(4)         NOT NULL,
+    product_name    VARCHAR(100)    NOT NULL,
+    product_type    VARCHAR(32)     NOT NULL,
+    sale_price      INTEGER,
+    purchase_price  INTEGER,
+    regist_date     DATE,
+    PRIMARY KEY (product_id)
+);
+
